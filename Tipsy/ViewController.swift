5e19c7675e9b3fbfcef308a359b93a4c6276b464
Rollin Francois <rfrancois21@hotmail.com>
2020-04-28 19:18:42 -0400

    Initial Commit

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Rollin Francois on 4/28/20.
+//  Copyright © 2020 Francois Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
