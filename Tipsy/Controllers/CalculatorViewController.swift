ca48ae63c362d744dc0985d5a9e0eec1caf3ed8d
Rollin Francois <rfrancois21@hotmail.com>
2020-04-28 22:34:16 -0400

    IBOutlets and IBActions connected,

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Rollin Francois on 4/28/20.
+//  Copyright © 2020 Francois Technology. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+    }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Rollin Francois on 4/28/20.
+//  Copyright © 2020 Francois Technology. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+    }
+    
+    
+}
